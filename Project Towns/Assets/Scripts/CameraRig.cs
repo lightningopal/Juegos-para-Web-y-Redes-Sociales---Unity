04fcd0ad6edaad40f792745cdf87a3db14e45478
Thund3rDev <samuelriosdev@gmail.com>
2020-12-09 18:32:50 +0100

    Cam

---
+++ Project Towns/Assets/Scripts/CameraRig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+    [Tooltip("Player's Transform")]
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = player.position;   
+    }
+}
