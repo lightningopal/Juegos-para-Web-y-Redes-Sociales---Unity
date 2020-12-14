fad030bbed759254757db71e5dd8f0a6a9d9d1d4
Thund3rDev <samuelriosdev@gmail.com>
2020-12-14 18:42:13 +0100

    Test WebGL

---
+++ Project Towns/Assets/Scripts/DebugScripts/FPSCounter.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public TextMeshProUGUI FPSCounterText;
+    private float nextFPSUpdate = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextFPSUpdate)
+        {
+            nextFPSUpdate = Time.time + 1.0f;
+            int fps = Mathf.RoundToInt(1 / Time.deltaTime);
+            FPSCounterText.text = "FPS: " + fps;
+        }
+    }
+}
