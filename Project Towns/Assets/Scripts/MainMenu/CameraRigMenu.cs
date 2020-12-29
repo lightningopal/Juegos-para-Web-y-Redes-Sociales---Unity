21aeb499f715f9e9f05351310aa4a1eed97bc2e1
Thund3rDev <samuelriosdev@gmail.com>
2020-12-29 17:54:02 +0100

    Cámara menú

---
+++ Project Towns/Assets/Scripts/MainMenu/CameraRigMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Clase CameraRigMenu, que controla el funcionamiento de la cámara en el menú
+/// </summary>
+public class CameraRigMenu : MonoBehaviour
+{
+    #region Variables
+    [Tooltip("Cantidad de rotación")]
+    [SerializeField]
+    private float rotationDelta = 4f;
+    #endregion
+
+    #region MétodosUnity
+    /// <summary>
+    /// Método Start, que se llama antes del primer frame
+    /// </summary>
+    void Start()
+    {
+        this.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+    }
+
+    /// <summary>
+    /// Método Update, que se llama cada frame
+    /// </summary>
+    void Update()
+    {
+        // Actualizar rotación
+        this.transform.Rotate(new Vector3(0, 1, 0), rotationDelta * Time.deltaTime);
+    }
+    #endregion
+}
